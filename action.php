03227cca5dbca5983803a480a7d4317771bda0b6
Thomas Kennedy <thomaskennedy1066@gmail.com>
2022-03-16 07:44:15 -0400

    cleaned up code

--- action.php
+++ action.php
@@ -1,8 +1,10 @@
 <?php
 
 if (($_POST['username']) == "select * from users" || ($_POST['password']) == "select * from users") {
-  echo "Success!  You hacked the second website, nice job! \r\n \r\n";
-  echo "Hold tight while others finish this exercise";
+  echo "Success!  You hacked the second website, nice job!";
+  echo "<br>";
+  echo "<br>";
+  echo "Hold tight while others finish this exercise.";
 } else {
   echo "Wrong username and password. Try again";
 }
