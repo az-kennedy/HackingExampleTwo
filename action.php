1c3cb56c3193e972d36ffaf5c17ca3378bed5986
Thomas Kennedy <thomaskennedy1066@gmail.com>
2022-03-16 07:56:58 -0400

    cleaned up more x2

--- action.php
+++ action.php
@@ -1,11 +1,14 @@
 <?php
 
-if (($_POST['username']) == "select * from users" || ($_POST['password']) == "select * from users") {
+if (strtolower(($_POST['username'])) == "select * from users" || strtolower(($_POST['password'])) == "select * from users") {
   echo "Success!  You hacked the second website, nice job!";
   echo "<br>";
   echo "<br>";
   echo "Hold tight while others finish this exercise.";
 } else {
-  echo "Wrong username and password. Try again";
+  echo "Sorry, wrong try again";
+  echo "<br>";
+  echo "<br>";
+  echo "Hint: Use SQL Injection";
 }
 ?>
